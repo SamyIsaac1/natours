4af0bea7e1b4ba4c6af87d5743c9dd43d8305ae6
Samy Isaac <60799236+SamyIsaac1@users.noreply.github.com>
2024-07-08 22:55:16 +0300

    feat: complete section 10

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const morgan = require('morgan');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,11 +15,53 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDELWARES
+
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-//  Body parsing
-app.use(express.json());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 3,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // custom middleware
@@ -23,6 +70,14 @@ app.use(express.static(`${__dirname}/public`));
 //   next();
 // });
 
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  // console.log(req.headers);
+  next();
+});
+
+// 3) ROUTES
 app.get('/', (req, res) => {
   res.status(200).json({ hello: 'hello' });
 });
@@ -38,10 +93,6 @@ app.use('/api/v1/users', userRouter);
 
 // Not Found
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't found ${req.originalUrl} on this server!`,
-  // });
   const err = new AppError(
     `Can't found ${req.originalUrl} on this server!`,
     404,
