80d5f36c1f9af0af311b2a3b76f916ac57c147e0
Samy Isaac <60799236+SamyIsaac1@users.noreply.github.com>
2024-07-18 14:49:18 +0300

    feat: addd missing Auth

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -8,12 +8,12 @@ const router = express.Router({ mergeParams: true });
 // POST /tour/1231545/reviews
 // Get /tour/1231545/reviews
 // POST /reviews
+router.use(authController.protect);
 
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserUds,
     reviewController.createReview,
@@ -22,7 +22,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview,
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview,
+  );
 
 module.exports = router;
