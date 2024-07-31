440204183c881da8ec571d97bf474753b7b8ea5e
Samy Isaac <60799236+SamyIsaac1@users.noreply.github.com>
2024-07-31 16:50:12 +0300

    feat: complete booking model

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
+const factory = require('./handlerFactory');
 
 const catchAsync = require('../utils/catchAsync');
 // const AppError = require('../utils/appError');
@@ -8,8 +10,6 @@ const catchAsync = require('../utils/catchAsync');
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
-
-  //   if (!tour) return new AppError('no tour found', 404);
 
   // 2) Create checkout session
   const session = await stripe.checkout.sessions.create({
@@ -36,9 +36,26 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       },
     ],
   });
+
   // 3) Create session as response
   res.status(200).json({
     status: 'success',
     session,
   });
 });
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only TEMPORARY, because it's UNSECURE: everyone can booking without paying
+  const { tour, user, price } = req.query;
+  if (!tour && !user && !price) return next();
+
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
+});
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
