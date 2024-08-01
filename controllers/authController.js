3221b3eee96e6567c2105c4325f6804213717708
Samy Isaac <60799236+SamyIsaac1@users.noreply.github.com>
2024-08-01 15:19:42 +0300

    feat:  add compression

--- controllers/authController.js
+++ controllers/authController.js
@@ -72,7 +72,7 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.logout = (req, res, next) => {
-  console.log('lgoout');
+  // console.log('lgoout');
   res.cookie('jwt', 'loggedout', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
--- public/js/stripe.js
+++ public/js/stripe.js
@@ -8,10 +8,8 @@ import { showAlert } from './alerts.js';
 export const bookTour = async (tourId) => {
   try {
     // 1) Get Checkout Session from API
-    const session = await axios(
-      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`,
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
     // 2) Create checkout form  charge credit card
     window.location.replace(session.data.session.url);
   } catch (error) {
